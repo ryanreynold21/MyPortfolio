58c30e43204d8aa262362b03ae606b025d74ab19
Lenovo <aunkyawin2035@gmail.com>
2023-07-23 21:34:55 +0630

    sidebarFinish

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
+  const data = [{
+    name:'Home',
+    to:'/'
+  },{
+    name:'About',
+    to:'/about'
+  },{
+    name:'Service',
+    to:'/service'
+  },{
+    name:'Portfolio',
+    to:'/portfolio'
+  },{
+    name:'News',
+    to:'/news'
+  },{
+    name:'Contact',
+    to:'/contact'
+  },]
   return (
-    <div className='  w-[450px] h-screen shadow-2xl fixed left-0 bg-[#494B50] z-50'>
-      Sidebar
+    <div className='  w-[440px] h-screen shadow-2xl fixed left-0 bg-[#494B50] z-50'>
+      <div className="">
+        {/* Sidebar Brand */}
+        <div className=" text-white text-[40px] font-bold hidden lg:block ml-28 mt-[100px]">
+          <h1>A K W</h1>
+        </div>
+        {/* sidebar */}
+        <div className=" mt-16">
+          <ul className=' text-[#BBBBBB] text-[20px] flex ml-28 flex-col gap-3'>
+            {data?.map(item => {
+            return(
+              <NavLink to={item.to}>
+            <li>{item.name}</li>
+              </NavLink>
+            )  
+            } )}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
