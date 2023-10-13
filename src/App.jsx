170eb37f29c347b1fc2e5ac034e17e1f991e927e
Lenovo <aunkyawin2035@gmail.com>
2023-10-13 15:14:45 +0630

    lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import "./App.css"
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Service from './pages/Service'
-import Portfolio from './pages/Portfolio'
-import News from './pages/News'
-import Contact from './pages/Contact'
-import Loader from './loader/Loader'
-import MouseFollower from './component/MouseFollower'
+import React, { Suspense, lazy } from 'react';
+import "./App.css";
+import { Route, Routes } from 'react-router-dom';
+import Loader from './loader/Loader';
+import MouseFollower from './component/MouseFollower';
 
 
-const App = () => {
-  const [panding,setPanding] = useState(true)
-  useEffect(() => {
-    setTimeout(() => {
-      setPanding(false)
-    }, 2500);
-  },[panding])
+const App = () =>
+{
+  const Home = lazy( () => import( './pages/Home' ) );
+  const About = lazy( () => import( './pages/About' ) );
+  const Service = lazy( () => import( './pages/Service' ) );
+  const Portfolio = lazy( () => import( './pages/Portfolio' ) );
+  const News = lazy( () => import( './pages/News' ) );
+  const Contact = lazy( () => import( './pages/Contact' ) );
+
   return (
     <>
-    <MouseFollower />
-    {panding && <Loader /> }
-    <Routes>
-      <Route path='/' element={ <Home /> } />
-      <Route path='/about' element={ <About /> } />
-      <Route path='/service' element={ <Service /> } />
-      <Route path='/portfolio' element={ <Portfolio /> } />
-      <Route path='/news' element={ <News /> } />
-      <Route path='/contact' element={ <Contact /> } />
-    </Routes>
+      <MouseFollower />
+      <Suspense fallback={ <Loader /> }>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/about' element={ <About /> } />
+          <Route path='/service' element={ <Service /> } />
+          <Route path='/portfolio' element={ <Portfolio /> } />
+          <Route path='/news' element={ <News /> } />
+          <Route path='/contact' element={ <Contact /> } />
+        </Routes>
+      </Suspense>
     </>
 
-    )
-}
+  );
+};
 
-export default App
+export default App;
